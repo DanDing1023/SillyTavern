523daa76c235afa55567f6ecbc301ba612c5d605
maver <kentucky@posteo.de>
2023-07-31 15:18:44 +0200

    Update existing prompts during migration
    Instead of adding new prompts for main, jailbreak and nsfw

--- public/script.js
+++ public/script.js
@@ -531,7 +531,7 @@ const system_messages = {
 };
 
 // Register configuration migrations
-registerPromptManagerMigration();
+registerPromptManagerMigration(saveSettings);
 
 $(document).ajaxError(function myErrorHandler(_, xhr) {
     if (xhr.status == 403) {
